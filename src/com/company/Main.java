0598c4345f5ce2e89a9fed763566a9cf6a0e382b
Nick Leat <nick.leat@trojans.dsu.edu>
2020-12-08 07:49:04 -0600

    Working on analyzing functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.Random;
 
 public class Main {
@@ -69,7 +71,9 @@ public class Main {
         double edgeCost[][] = new double[vert][vert];
         boolean placedVert[] = new boolean[vert];
         double vertexLocation[][] = new double[vert][2];
-        double theta = 360/vert;
+        int varplace[] = new int[vert];
+        boolean allfill = false;
+        double theta = (Math.PI*2)/vert;
 
         for(int i = 0; i < vert; i++){
             placedVert[i] = false;
@@ -78,15 +82,30 @@ public class Main {
         //calculate the graph position equally around a circle
         for(int i = 0; i < vert; i++) {
 
-            vertexLocation[randVert][0] = (radius * Math.cos(theta * randVert));//set x location
-            vertexLocation[randVert][1] = (radius * Math.sin(theta * randVert));//set y location
+            vertexLocation[randVert][0] = (radius * Math.cos(theta * i));//set x location
+            vertexLocation[randVert][1] = (radius * Math.sin(theta * i));//set y location
+            System.out.printf("%2d ", randVert);
+            System.out.printf(": x: %10.1f  y:   %10.1f ", vertexLocation[randVert][0],vertexLocation[randVert][1]);
+            System.out.println(" ");
+            varplace[i] = randVert;
             placedVert[randVert] = true;
             if(randVert == vert){
                 vert--;
             }
+            for(int j= 0; j < vert; j++){
+                if(placedVert[j] == false){
+                    allfill = false;
+                    break;
+                }else{
+                    allfill = true;
+                }
+            }
+            if(allfill == true){
+                break;
+            }
             randVert = rand.nextInt(vert);
             if(placedVert[randVert] == true) {
-                while (placedVert[randVert] == false) {
+                while (placedVert[randVert] == true) {
                     randVert = rand.nextInt(vert);
                 }
             }
@@ -95,21 +114,22 @@ public class Main {
         for(int i = 0; i < vert; i++){
             for(int j = 0; j < vert; j++){
                 if(i != j){
-                    edgeCost[i][j] = Math.pow(vertexLocation[i][1] - vertexLocation[j][1],2) + Math.pow(vertexLocation[i][0] - vertexLocation[j][0],2);
-                    if(edgeCost[i][j] < 0){
-                        edgeCost[i][j] = edgeCost[i][j] * -1;
-                    }
+                    edgeCost[i][j] = Math.pow(vertexLocation[j][1] - vertexLocation[i][1],2) + Math.pow(vertexLocation[j][0] - vertexLocation[i][0],2);
                     edgeCost[i][j] = Math.sqrt(edgeCost[i][j]);
                     edgeCost[j][i] = edgeCost[i][j];
                 }
             }
         }
 
-
+        for(int i = 0; i < vert; i++){
+            System.out.printf("%3d ",varplace[i]);
+        }
+        System.out.println(" ");
+        System.out.printf("Cost: %.2f\n", calccost(edgeCost,varplace,vert));
         return edgeCost;
     }
 //************************************Greedy Algorithm*************************
-     public static void greedy(double edgeCost[][], int vert){
+     public static double greedy(double edgeCost[][], int vert){
         int next = 0;
         int current = 0;
         int set = 0;
@@ -177,12 +197,13 @@ public class Main {
         }
         System.out.println("   0 ");
         System.out.printf("Num verts: %d\n", vert);
-        System.out.printf("Cost: %.2f\n", cost);
+        System.out.printf("Cost: %.2f\n", cost);/**/
 
+         return cost;
      }
 //*****************************************************************************
 //*******************************Brute Force Algorithm*************************
-    public static void bruteForce(double edgeCost[][], int vert){
+    public static double bruteForce(double edgeCost[][], int vert){
         long val = 1;
         for(int i = 2; i < vert-1; i++){
             val = val * i;
@@ -208,9 +229,9 @@ public class Main {
         }
         System.out.println("   0 ");
         System.out.printf("Num verts: %d\n", vert);
-        System.out.printf("Cost: %.2f\n", cost);
+        System.out.printf("Cost: %.2f\n", cost);/**/
+        return cost;
     }
-
     public static int[] permutation(double edgeCost[][], int[] nodes, int l, int r,int vert){
         double cost = calccost(edgeCost,nodes,vert);
         if(l == r){
@@ -267,11 +288,15 @@ public class Main {
 //**************************************Dynamic Algorithm**********************
     public static void dynamic(double edgeCost[][], int vert){
         int travelList[] = new int[vert];
-        int cost = 0;
+        for(int i = 0; i < vert; i++){
+            travelList[i] = i;
+        }
+        LowestCost = calccost(edgeCost,travelList,vert);
 
+        travelList = permutation(edgeCost,travelList,1,vert-1,vert);
+        double cost = LowestCost;
 
-
-        System.out.println("Brute Force: ");
+        System.out.println("Dynamic algorithm: ");
         for(int i = 0; i < vert; i++){
             System.out.printf("%3d ",travelList[i]);
         }
@@ -280,19 +305,97 @@ public class Main {
         System.out.printf("Cost: %.2f\n", cost);
     }
 //*****************************************************************************
+    public static void testingExact(){
+        int vert = 3;
+        double time[] = new double[20];
+        int count = 1;
+        int N[] = new int[20];
+        System.out.printf("N(Verts)          Time\n");
+
+        while(count <=20){
+            double edgeCost[][] = new double[vert][vert];
+            edgeCost = GenerateRandomCostMatrix(vert,1000);
+
+            double before = getCpuTime();
+            bruteForce(edgeCost,vert);
+            double after = getCpuTime();
+            N[count-1] = vert;
+            time[count-1] = after - before;
+            System.out.printf("%8d      %4.2f\n",N[count-1], time[count-1]);
+            count++;
+            vert++;
+        }
+
+
+    }
+
+    public static void timeGreedy(){
+        int vert = 50;
+        double time[] = new double[50];
+        int count = 1;
+        int N[] = new int[50];
+        System.out.printf("N(Verts)          Time\n");
+        boolean cont = true;
+
+        while(cont == true){
+            double edgeCost[][] = new double[vert][vert];
+            edgeCost = GenerateRandomCircularGraphCostMatrix(vert,1000);
+
+            double before = getCpuTime();
+            greedy(edgeCost,vert);
+            double after = getCpuTime();
+            N[count-1] = vert;
+            time[count-1] = after - before;
+            System.out.printf("%8d      %4.2f\n",N[count-1], time[count-1]);
+            count++;
+            vert = vert * 2;
+            if(time[count-2] > 1000000000){
+                cont = false;
+            }
+        }
+    }
+    public static void calcSQR(){
+        int vert = 4;
+        int cont = 0;
+        double costbf = 0;
+        double costg = 0;
+        double SQR[] = new double[9];
+        while(cont < 9){
+            double edgeCost[][] = new double[vert][vert];
+            edgeCost = GenerateRandomCostMatrix(vert,1000);
+
+            for(int i = 0; i < 5; i++) {
+                costbf += bruteForce(edgeCost, vert);
+                costg += greedy(edgeCost, vert);
+            }
+            costbf = costbf/5;
+            costg = costg/5;
+            SQR[cont] = costg/costbf;
+            vert++;
+            cont++;
+
+        }
+        for(int i = 0; i < 9; i++){
+            System.out.printf("%6.2f",SQR[i]);
+        }
+        System.out.println(" ");
+
+    }
     public static void main(String[] args) {
         Random rand = new Random();
         int vert = rand.nextInt(15);
         vert++;
         double edgeCost[][] = new double[vert][vert];
 
-
-        edgeCost = GenerateRandomCostMatrix(vert,1000);
+        //timeGreedy();
+        //edgeCost = GenerateRandomCostMatrix(vert,1000);
         //edgeCost = GenerateRandomEuclideanCostMatrix(vert,1000);
         //edgeCost = GenerateRandomCircularGraphCostMatrix(vert,1000);
-        greedy(edgeCost,vert);
-        bruteForce(edgeCost,vert);
+        //greedy(edgeCost,vert);
+        //bruteForce(edgeCost,vert);
+        calcSQR();;
 
+        //testingExact();
         for(int i = 0; i < vert; i++){
             for(int j = 0; j < vert; j++){
                 System.out.printf("%6.1f ", edgeCost[i][j]);
@@ -302,5 +405,14 @@ public class Main {
 
 
         return;
+    }
+    public static long getCpuTime( ) {
+
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+
+        return ((ThreadMXBean) bean).isCurrentThreadCpuTimeSupported( ) ?
+
+                bean.getCurrentThreadCpuTime( ) : 0L;
+
     }
 }
